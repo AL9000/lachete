3500ed766388b87795ee215a63788b89e0ae0ebe
Ganapati <ganapati@kalkulators.org>
2015-03-26 21:05:33 +0100

    add link to gps in main menu

--- gps/urls.py
+++ gps/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, url
 from gps.views import GPSView
 
 urlpatterns = patterns('',
-    url(r'^$', GPSView.as_view(), name="gps"),
+    url(r'^$', GPSView.as_view(), name="index"),
 )
