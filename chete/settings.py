125edfbe10eb42abc15b14fd8105a512d7b9ef0f
Ganapati <ganapati@kalkulators.org>
2015-03-26 13:23:25 +0100

    move tpl and view to website app

--- chete/settings.py
+++ chete/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'chete',
     'sondages',
+    'website',
 )
 
 MIDDLEWARE_CLASSES = (
--- chete/urls.py
+++ chete/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from chete.views import HomeView
 
 urlpatterns = patterns('',
-    url(r'^$', HomeView.as_view(), name="home"),
+    url(r'^$', include('website.urls', namespace="website")),
     url(r'^sondages/', include('sondages.urls', namespace="sondages")),
     url(r'^admin/', include(admin.site.urls)),
 )
