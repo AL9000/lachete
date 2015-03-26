01adc7a0cecb93a3bb536cb0efa809c01d0d1163
Ganapati <ganapati@kalkulators.org>
2015-03-26 11:39:20 +0100

    add base app chete to INSTALLED_APPS + change template management

--- chete/settings.py
+++ chete/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'chete',
     'sondages',
 )
 
@@ -88,4 +89,4 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 
-TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
+TEMPLATE_DIRS = (os.path.join(BASE_DIR),)
