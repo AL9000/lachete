ddc73c51970d9220913799b26eaf740467f02cf2
Al-Ouest <alouest_44@yahoo.fr>
2015-04-01 15:23:18 +0200

    Registration and auth
    Users can register and login/logout

--- chete/urls.py
+++ chete/urls.py
@@ -2,14 +2,10 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^$', include('website.urls', namespace="website")),
+    url(r'^', include('website.urls', namespace="website")),
 
     url(r'^gps/', include('gps.urls', namespace="gps")),
     url(r'^sondages/', include('sondages.urls', namespace="sondages")),
     url(r'^blog/', include('blog.urls', namespace="blog")),
     url(r'^admin/', include(admin.site.urls)),
-
-    # TODO Utiliser le parametre next pour rediriger l'utilisateur à la page souhaitée initialement
-    url(r'^accounts/login/', 'django.contrib.auth.views.login'),
-    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
 )
