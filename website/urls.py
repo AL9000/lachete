8d7d7a70cac61d456c1055b2e4e39e7d560979bf
Al-Ouest <alouest_44@yahoo.fr>
2015-04-10 18:10:35 +0200

    Improvements on website
    Adding organisation page.
    Look&Feel

--- website/urls.py
+++ website/urls.py
@@ -1,17 +1,20 @@
-from django.conf.urls import patterns, url, include
+from django.conf.urls import patterns, url
 from website.views import HomeView
 from django.views.generic.edit import CreateView
+from django.views.generic import TemplateView
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import permission_required
 
 urlpatterns = patterns('',
     url(r'^$', HomeView.as_view(), name="home"),
-    url('^register/', CreateView.as_view(
-        template_name='registration/register.html',
-        form_class=UserCreationForm,
-        success_url='/'),
+    url(r'^register/',
+        CreateView.as_view(template_name='registration/register.html',
+                           form_class=UserCreationForm,
+                           success_url='/'),
         name='register'),
+    url(r'^organisation/', permission_required('is_staff')(TemplateView.as_view(template_name='website/sheets.html'), ),
+        name='organisation'),
 
-    # TODO Utiliser le parametre next pour rediriger l'utilisateur à la page souhaitée initialement après le login
-    url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login',),
     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
 )
--- website/views.py
+++ website/views.py
@@ -1,18 +1,21 @@
 from django.views import generic
 from sondages.models import Question
 from blog.models import Article
+from django.utils import timezone
 
 
 class HomeView(generic.ListView):
     model = Question
-    template_name = "home.html"
+    template_name = "website/templates/website/home.html"
 
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
-        context['last_question'] = Question.objects.filter(
-            open=True
-        ).latest('pub_date')
-        context['last_article'] = Article.objects.filter(
-            ouvert=True
-        ).latest('date_de_parution')
+        if Question.objects.all().exists():
+            context['last_question'] = Question.objects.filter(
+                open=True,
+                pub_date__lte=timezone.now(),
+            ).latest('pub_date')
+            context['last_article'] = Article.objects.filter(
+                ouvert=True
+            ).latest('date_de_parution')
         return context
