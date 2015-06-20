5bfe73eda0cb24a83a05a203f9833ab7594943a4
Al-Ouest <alouest_44@yahoo.fr>
2015-06-20 15:51:17 +0200

    fix is_staff bug on template view of website/sheets.html

--- website/urls.py
+++ website/urls.py
@@ -3,7 +3,7 @@ from website.views import HomeView
 from django.views.generic.edit import CreateView
 from django.views.generic import TemplateView
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.decorators import permission_required
+from django.contrib.admin.views.decorators import staff_member_required
 
 urlpatterns = patterns('',
     url(r'^$', HomeView.as_view(), name="home"),
@@ -12,8 +12,7 @@ urlpatterns = patterns('',
                            form_class=UserCreationForm,
                            success_url='/'),
         name='register'),
-    url(r'^organisation/', permission_required('is_staff')(TemplateView.as_view(template_name='website/sheets.html'), ),
-        name='organisation'),
+    url(r'^organisation/', staff_member_required(TemplateView.as_view(template_name='website/sheets.html'), ), name='organisation'),
 
     url(r'^accounts/login/$', 'django.contrib.auth.views.login',),
     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
